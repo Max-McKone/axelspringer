85a51c5072553b0affd2190f0fa9679b0371d6f3
beta <beta.nycz@icloud.com>
2024-05-30 17:57:21 +0200

    bux

---
+++ beta_ex/random.js
@@ -0,0 +1,19 @@
+// Update your HTML document to include a button labeled "Generate
+// Random Number" and a paragraph element where the random number will
+// be displayed.
+// ● Write a JavaScript function that generates a random number using the
+// Math.random() method and displays it in the designated paragraph.
+// ● Add an event listener to the button element so that when it is clicked, the
+// JavaScript function is executed.
+// ● Test the functionality by clicking the button to generate a random number
+// and verify that it changes with each click.
+
+
+function generateRandom() {
+  alert("Nazar");
+}
+
+
+function displayMessage(message) {
+  document.getElementById("messageText").innerHTML = message;
+}
