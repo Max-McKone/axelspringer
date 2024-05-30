a3497cfa4b7a72a9cb446646349897925f5ff02f
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-05-30 16:57:11 +0200

    Delete 0530

--- Estelle-E14/javascript/exercise_4.js
+++
@@ -1,9 +0,0 @@
-/* <button id="generateNumber">Generate Random Number</button>
-<div id="resultBox"></div> */
-
-const buttonElement = document.getElementById("generateNumberButton");
-
-buttonElement.addEventListener('click', function () {
-  const randomNum = Math.floor(Math.random() * 9999);
-  resultBox.innerHTML = `Your number has been generated and it is: ${randomNum}.<br>If you don't like it, press again!`;
-  })
